c7c43da9a0f3cb5f52f49e906baf2a5c46985726
pc3090 <ilikeniku@icloud.com>
2022-02-06 12:19:34 +0900

    update

--- src/modules/common/tokenizers/pp_add.py
+++ src/modules/common/tokenizers/pp_add.py
@@ -80,8 +80,7 @@ class PPAddTokenizer(TokenizerBase):
             elif a2 == 1 and a2_next == 2:
                 prosody.append("[")
             else:
-                if n != 1 and n != N - 2:
+                if prosody[-1] != '$':
                     prosody.append('_')
-            assert len(phoneme) == len(prosody), f'\n{phoneme}\n{prosody}'
 
         return phoneme, prosody
