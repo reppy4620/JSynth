d431591d0961f276651a0693f3fc9e30b8514803
pc3090 <ilikeniku@icloud.com>
2022-02-06 13:40:43 +0900

    update

--- src/validate.py
+++ src/validate.py
@@ -1,5 +1,12 @@
 from argparse import ArgumentParser
-from modules.from_x import validate_from_args
+from omegaconf import OmegaConf
+
+from modules.conformer import validate as cfm_validate
+
+
+validate_fn = {
+    'conformer': cfm_validate
+}
 
 
 def main():
@@ -8,7 +15,8 @@ def main():
     parser.add_argument('--ckpt_path', type=str, required=True)
     parser.add_argument('--output_dir', type=str, required=True)
     args = parser.parse_args()
-    validate_from_args(args)
+    config = OmegaConf.load(args.config)
+    validate_fn[config.name](args, config)
 
 
 if __name__ == '__main__':
