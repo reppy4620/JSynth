751cda7d44c4ef7bfa7ef4591bbe0463c5bcc965
pc3090 <ilikeniku@icloud.com>
2022-02-06 21:09:24 +0900

    update

--- src/modules/conformer/trainer.py
+++ src/modules/conformer/trainer.py
@@ -104,7 +104,7 @@ class Trainer:
                         pitch,
                         energy
                     ) = batch
-                    o = self.model([*labels, x_length])
+                    o = model([*labels, x_length])
                     plt.figure(figsize=(8, 6))
                     plt.subplot(2, 1, 1)
                     plt.imshow(o[0].squeeze().detach().cpu().numpy(), aspect='auto', origin='lower')
