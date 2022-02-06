287992f54d2b3fcb9b25b7d1d8282b342fc925dc
pc3090 <ilikeniku@icloud.com>
2022-02-06 20:06:05 +0900

    update

--- src/modules/common/model/layers.py
+++ src/modules/common/model/layers.py
@@ -18,40 +18,40 @@ class TTSLearnEmbeddingLayer(nn.Module):
 
 
 class PAFEmbeddingLayer(nn.Module):
-    # def __init__(self, n_p, n_a, n_f, channels):
-    #     super(PAFEmbeddingLayer, self).__init__()
-    #     self.scale = math.sqrt(channels)
-    #
-    #     self.p_emb = nn.Embedding(n_p, channels)
-    #     nn.init.normal_(self.p_emb.weight, 0.0, channels ** -0.5)
-    #
-    #     self.a_emb = nn.Embedding(n_a, channels)
-    #     nn.init.normal_(self.a_emb.weight, 0.0, channels ** -0.5)
-    #
-    #     self.f_emb = nn.Embedding(n_f, channels)
-    #     nn.init.normal_(self.f_emb.weight, 0.0, channels ** -0.5)
-    #
-    # def forward(self, p, a, f):
-    #     p = self.p_emb(p) * self.scale
-    #     a = self.a_emb(a) * self.scale
-    #     f = self.f_emb(f) * self.scale
-    #     x = torch.cat([p, a, f], dim=-1).transpose(-1, -2)
-    #     return x
-    def __init__(self, n_p, n_f, channels, **kwargs):
+    def __init__(self, n_p, n_a, n_f, channels):
         super(PAFEmbeddingLayer, self).__init__()
         self.scale = math.sqrt(channels)
 
-        self.phoneme_emb = nn.Embedding(n_p, channels)
-        self.f2_emb = nn.Embedding(n_f, channels)
-        nn.init.normal_(self.phoneme_emb.weight, 0.0, channels ** -0.5)
-        nn.init.normal_(self.f2_emb.weight, 0.0, channels ** -0.5)
+        self.p_emb = nn.Embedding(n_p, channels)
+        nn.init.normal_(self.p_emb.weight, 0.0, channels ** -0.5)
 
-    def forward(self, phoneme, a1, f2):
-        phoneme_emb = self.phoneme_emb(phoneme) * self.scale
-        f2_emb = self.f2_emb(f2) * self.scale
-        a1_emb = a1.unsqueeze(-1).expand(-1, -1, phoneme_emb.size(-1))
-        x = torch.cat([phoneme_emb, f2_emb, a1_emb], dim=-1).transpose(-1, -2)
+        self.a_emb = nn.Embedding(n_a, channels)
+        nn.init.normal_(self.a_emb.weight, 0.0, channels ** -0.5)
+
+        self.f_emb = nn.Embedding(n_f, channels)
+        nn.init.normal_(self.f_emb.weight, 0.0, channels ** -0.5)
+
+    def forward(self, p, a, f):
+        p = self.p_emb(p) * self.scale
+        a = self.a_emb(a) * self.scale
+        f = self.f_emb(f) * self.scale
+        x = torch.cat([p, a, f], dim=-1).transpose(-1, -2)
         return x
+    # def __init__(self, n_p, n_f, channels, **kwargs):
+    #     super(PAFEmbeddingLayer, self).__init__()
+    #     self.scale = math.sqrt(channels)
+    #
+    #     self.phoneme_emb = nn.Embedding(n_p, channels)
+    #     self.f2_emb = nn.Embedding(n_f, channels)
+    #     nn.init.normal_(self.phoneme_emb.weight, 0.0, channels ** -0.5)
+    #     nn.init.normal_(self.f2_emb.weight, 0.0, channels ** -0.5)
+    #
+    # def forward(self, phoneme, a1, f2):
+    #     phoneme_emb = self.phoneme_emb(phoneme) * self.scale
+    #     f2_emb = self.f2_emb(f2) * self.scale
+    #     a1_emb = a1.unsqueeze(-1).expand(-1, -1, phoneme_emb.size(-1))
+    #     x = torch.cat([phoneme_emb, f2_emb, a1_emb], dim=-1).transpose(-1, -2)
+    #     return x
 
 
 class PPAddEmbeddingLayer(nn.Module):
