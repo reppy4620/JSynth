9506b055c159873726006ff9035e42043b4f4bcd
pc3090 <ilikeniku@icloud.com>
2022-02-07 23:00:46 +0900

    update

--- src/modules/common/model/layers.py
+++ src/modules/common/model/layers.py
@@ -37,21 +37,6 @@ class PAFEmbeddingLayer(nn.Module):
         f = self.f_emb(f) * self.scale
         x = torch.cat([p, a, f], dim=-1).transpose(-1, -2)
         return x
-    # def __init__(self, n_p, n_f, channels, **kwargs):
-    #     super(PAFEmbeddingLayer, self).__init__()
-    #     self.scale = math.sqrt(channels)
-    #
-    #     self.phoneme_emb = nn.Embedding(n_p, channels)
-    #     self.f2_emb = nn.Embedding(n_f, channels)
-    #     nn.init.normal_(self.phoneme_emb.weight, 0.0, channels ** -0.5)
-    #     nn.init.normal_(self.f2_emb.weight, 0.0, channels ** -0.5)
-    #
-    # def forward(self, phoneme, a1, f2):
-    #     phoneme_emb = self.phoneme_emb(phoneme) * self.scale
-    #     f2_emb = self.f2_emb(f2) * self.scale
-    #     a1_emb = a1.unsqueeze(-1).expand(-1, -1, phoneme_emb.size(-1))
-    #     x = torch.cat([phoneme_emb, f2_emb, a1_emb], dim=-1).transpose(-1, -2)
-    #     return x
 
 
 class PPAddEmbeddingLayer(nn.Module):
--- src/modules/common/tokenizers/paf.py
+++ src/modules/common/tokenizers/paf.py
@@ -29,18 +29,6 @@ class PAFTokenizer(TokenizerBase):
         is_transpose = [0, 0, 0]
         return p_id, a_id, f_id, is_transpose
 
-        # p_id = [self.p_dict[s] for s in p]
-        #
-        # a_id = [a[i + 1] if i == 0 and a1 == 'xx' else a[i - 1] if a1 == 'xx' else a1 for i, a1 in enumerate(a)]
-        # a_id = [int(a1) / 15 for a1 in a_id]
-        #
-        # f_id = [self.f_dict[x] for x in f]
-        #
-        # p_id = torch.LongTensor(p_id)
-        # a_id = torch.FloatTensor(a_id)
-        # f_id = torch.LongTensor(f_id)
-        # return p_id, a_id, f_id, [0, 0, 0]
-
     @staticmethod
     def load_dictionary():
         dictionary = dict()
