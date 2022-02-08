4dcce4776fe6d8923253984fe545a27e09df7d8b
pc3090 <ilikeniku@icloud.com>
2022-02-08 21:38:16 +0900

    update

--- src/modules/conformer/pl.py
+++ src/modules/conformer/pl.py
@@ -52,11 +52,13 @@ class ConformerModule(LightningModule):
                 energy
             ) = batch
             o = self.model([*labels, x_length])
+            o = o[0][:, :y_length[0]]
+            y = y[0][:, :y_length[0]]
             plt.figure(figsize=(8, 6))
             plt.subplot(2, 1, 1)
-            plt.imshow(o[0].detach().cpu().float().numpy(), aspect='auto', origin='lower')
+            plt.imshow(o.detach().cpu().float().numpy(), aspect='auto', origin='lower')
             plt.subplot(2, 1, 2)
-            plt.imshow(y[0].detach().cpu().float().numpy(), aspect='auto', origin='lower')
+            plt.imshow(y.detach().cpu().float().numpy(), aspect='auto', origin='lower')
             plt.savefig(f'{self.params.output_dir}/latest.png')
             plt.close()
 
