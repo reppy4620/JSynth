93cae90b9f30f64db2fdd31e85c93e93a34735bf
pc3090 <ilikeniku@icloud.com>
2022-02-18 19:51:05 +0900

    update

--- src/modules/conformer/model/model.py
+++ src/modules/conformer/model/model.py
@@ -33,9 +33,9 @@ class ConformerModel(ModelBase):
         x = self.emb(*labels)
         x, pos_emb = self.relative_pos_emb(x)
 
-        print(x.size(), pos_emb.size())
-
         x_mask = sequence_mask(x_length).unsqueeze(1).to(x.dtype)
+
+        print(x.size(), pos_emb.size(), x_mask.size())
         x = self.encoder(x, pos_emb, x_mask)
 
         x, y_mask = self.variance_adopter.infer(x, x_mask)
