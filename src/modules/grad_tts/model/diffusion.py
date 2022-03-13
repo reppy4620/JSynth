58ed7b09eebf62a3e366d0319ba55d03be4b91d0
pc3090 <ilikeniku@icloud.com>
2022-03-13 21:57:02 +0900

    update

--- src/modules/grad_tts/model/diffusion.py
+++ src/modules/grad_tts/model/diffusion.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 
 from scipy.integrate import solve_ivp
 from einops import rearrange
-from ...common.model.layers.common import LayerNorm
 
 
 class Upsample(nn.Module):
@@ -24,6 +23,20 @@ class Downsample(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, channels, eps=1e-5):
+        super().__init__()
+        self.eps = eps
+
+        self.g = nn.Parameter(torch.ones(1, channels, 1, 1))
+        self.b = nn.Parameter(torch.zeros(1, channels, 1, 1))
+
+    def forward(self, x):
+        mean = torch.mean(x, dim=1, keepdim=True)
+        var = torch.var(x, dim=1, unbiased=False, keepdim=True)
+        return (x - mean) / (var + self.eps).sqrt * self.g + self.b
 
 
 class PreNorm(nn.Module):
