bfefb3fcee34b27c30b5e211ebd3c3033f39de30
pc3090 <ilikeniku@icloud.com>
2022-03-13 21:58:13 +0900

    update

--- src/modules/grad_tts/model/diffusion.py
+++ src/modules/grad_tts/model/diffusion.py
@@ -36,7 +36,7 @@ class LayerNorm(nn.Module):
     def forward(self, x):
         mean = torch.mean(x, dim=1, keepdim=True)
         var = torch.var(x, dim=1, unbiased=False, keepdim=True)
-        return (x - mean) / (var + self.eps).sqrt * self.g + self.b
+        return (x - mean) / (var + self.eps).sqrt() * self.g + self.b
 
 
 class PreNorm(nn.Module):
